64189a888bab671858f73607cdebe02341dc760c
albicant <silvics@protonmail.com>
2020-02-19 13:26:09 -0800

    Project 4: Fixed documentation

--- airline/src/main/java/edu/pdx/cs410J/sytov/Converter.java
+++ airline/src/main/java/edu/pdx/cs410J/sytov/Converter.java
@@ -1,7 +1,15 @@
 package edu.pdx.cs410J.sytov;
 
+/**
+ * The Converter class that converts the representation of an Airline in a text file to an XML file.
+ */
 public class Converter {
 
+    /**
+     * The main method for the Converter class that parses command line arguments.
+     * Expects to get text_file_name as the first argument and xml_file_name as the second argument
+     * @param args is a string of console arguments
+     */
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("Error: Missing command line arguments!");
@@ -21,7 +29,7 @@ public class Converter {
             try {
                 airline = tp.parse();
             } catch (Exception e) {
-                System.err.println("Cannot create airline from the file");
+                System.err.println("Cannot create airline from the fil");
                 System.exit(1);
             }
         } else {
