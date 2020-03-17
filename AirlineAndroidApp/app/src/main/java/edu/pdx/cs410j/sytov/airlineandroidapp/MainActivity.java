23ee59b24d33ab51ba4d917e122882852d95bdf5
albicant <silvics@protonmail.com>
2020-03-16 19:17:54 -0700

    Project 6: finished the MVP

--- AirlineAndroidApp/app/src/main/java/edu/pdx/cs410j/sytov/airlineandroidapp/MainActivity.java
+++ AirlineAndroidApp/app/src/main/java/edu/pdx/cs410j/sytov/airlineandroidapp/MainActivity.java
@@ -2,11 +2,8 @@ package edu.pdx.cs410j.sytov.airlineandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,18 +14,57 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Collection;
 
+import edu.pdx.cs410J.AirportNames;
 import edu.pdx.cs410J.ParserException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Airline airline;
+    private AirportNames air_names;
 //    private EditText et_airline_name;
+
+    private boolean verifyAirportCode(String code) {
+        if(this.air_names.getName(code) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean verifyDateTime(String time) {
+        if (!time.matches("[/: 0-9]+(am|pm)$"))
+            return false;
+        String[] tokens = time.split(" ");
+        if (tokens.length != 3)
+            return false;
+        String[] tokens_date = tokens[0].split("/");
+        String[] tokens_time = tokens[1].split(":");
+        if(tokens_date.length != 3 || tokens_time.length != 2)
+            return false;
+        if(tokens_date[0].length() < 1 || tokens_date[0].length() > 2 ||
+                tokens_date[1].length() < 1 || tokens_date[1].length() > 2 ||
+                tokens_date[2].length() != 4 ||
+                tokens_time[0].length() < 1 || tokens_time[0].length() > 2 ||
+                tokens_time[1].length() != 2) {
+            return false;
+        }
+        if(Integer.parseInt(tokens_date[0]) < 1 || Integer.parseInt(tokens_date[0]) > 12 ||
+                Integer.parseInt(tokens_date[1]) < 1 || Integer.parseInt(tokens_date[1]) > 31 ||
+                Integer.parseInt(tokens_date[2]) < 1900 ||
+                Integer.parseInt(tokens_time[0]) < 1 || Integer.parseInt(tokens_time[0]) > 12 ||
+                Integer.parseInt(tokens_time[1]) < 0 || Integer.parseInt(tokens_time[1]) > 59) {
+            return false;
+        }
+        if(!tokens[2].equals("am") && !tokens[2].equals("pm"))
+            return false;
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.create_airline);
 
         File dir = getFilesDir();
         TextParser tp = new TextParser(dir,"airline.txt");
@@ -51,8 +87,6 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.add_flight);
             TextView airline_name = findViewById(R.id.airline_name);
             airline_name.setText("\"" + this.airline.getName() + "\"");
-//            this.et_airline_name.setEnabled(false);
-//            this.et_airline_name.setInputType(InputType.TYPE_NULL);
         }
     }
 
@@ -60,20 +94,16 @@ public class MainActivity extends AppCompatActivity {
         EditText et_airline_name = findViewById(R.id.airlineName);
         String airline_name = et_airline_name.getText().toString();
         if (!airline_name.equals("") && airline_name != null) {
-            this.createAirline(airline_name);
+            this.createAirline(view, airline_name);
+            setContentView(R.layout.activity_main);
         }
         else {
-            this.displayErrorMessage(view, "Invalid airline name");
-//            Snackbar.make(view, "Invalid airline name", 10000).show();
+            this.displayPopupMessage(view, "Invalid airline name");
         }
 
-        setContentView(R.layout.activity_main);
-
-//        EditText new_airline_name = findViewById(R.id.newAirlineName);
-//        new_airline_name.setText(this.airline.getName());
     }
 
-    private void displayErrorMessage(View view, String msg) {
+    private void displayPopupMessage(View view, String msg) {
         Snackbar.make(view, msg, 10000).show();
     }
 
@@ -85,13 +115,13 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
     }
 
-    private void createAirline(String airline_name) {
+    private void createAirline(View view, String airline_name) {
         if (this.airline == null) {
             this.airline = new Airline(airline_name);
         }
-        // else display error
-        // add View view to the function argument
-        // Snackbar.make(view, "I was clicked", 1000).show();
+        else {
+            displayPopupMessage(view, "Cannot create the airline. An airline already exist!");
+        }
     }
 
     public void addFlightToAirline(View view) {
@@ -106,45 +136,55 @@ public class MainActivity extends AppCompatActivity {
         try {
             number = Integer.parseInt(str_number);
         } catch (Exception e) {
-            // popup error message
+            displayPopupMessage(view, "Flight number must be an integer!");
+            return;
         }
         String src = et_src.getText().toString();
+        if (!verifyAirportCode(src)) {
+            displayPopupMessage(view, "Invalid airport source code!");
+            return;
+        }
         String depart = et_depart.getText().toString();
+        if (!verifyDateTime(depart)) {
+            displayPopupMessage(view, "Invalid departure time! Time must use the following format: MM/dd/yyyy hh:mm a");
+            return;
+        }
         String dest = et_dest.getText().toString();
+        if (!verifyAirportCode(dest)) {
+            displayPopupMessage(view, "Invalid airport destination code!");
+            return;
+        }
         String arrive = et_arrive.getText().toString();
-        // add verification for each variable
+        if (!verifyDateTime(arrive)) {
+            displayPopupMessage(view, "Invalid arrival time! Time must use the following format: MM/dd/yyyy hh:mm a");
+            return;
+        }
 
         Flight flight = null;
         try {
             flight = new Flight(number, src, depart, dest, arrive);
         }
         catch (Exception e) {
-            // popup error message
+            displayPopupMessage(view, "Cannot create the flight.");
+            return;
         }
         if (flight != null) {
             this.airline.addFlight(flight);
         }
 
         setContentView(R.layout.activity_main);
-        //popup sucsess message
+        displayPopupMessage(view, "You have scornfully added the flight #" + str_number + " to airline \"" + this.airline.getName() + "\".");
     }
-
-//    private boolean verifyArgument() {
-//
-//    }
-
-    public void addFlight(int number, String src, String depart, String dest, String arrive) throws IllegalArgumentException {
-        Flight flight = new Flight(number, src, depart, dest, arrive);
-        if (this.airline != null) {
-            this.airline.addFlight(flight);
-        }
-        //else display error
-//            Snackbar.make
-    }
-
 
     public void printReadme(View view) {
         setContentView(R.layout.readme);
+        TextView tv = findViewById(R.id.printReadMe);
+        String str = "This project provides the following functionality:\n\n" +
+                "-\"HELP\" prints the README.\n\n" +
+                "-\"ADD A FLIGHT\" allows the user to create a new airline or create a new flight.\n\n" +
+                "-\"SEARCH FLIGHTS\" allows the user to display flights that start and end at the certain airports. If no airports are provided, all of the airline's flights will be displayed.\n\n" +
+                "-\"DELETE AIRLINE\" allows the user to delete the airline.";
+        tv.setText(str);
     }
 
     public void mainMenu(View view) {
@@ -152,42 +192,85 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void deleteAirline(View view) {
+        if (this.airline == null) {
+            displayPopupMessage(view, "The airline you are trying to delete does not exist.");
+            return;
+        }
+        String name = this.airline.getName();
         this.airline = null;
-        // popup an approrpiate message
+        displayPopupMessage(view, "You have successfully deleted \"" + name + "\" airline.");
+    }
+
+    public void goToSearchFlightsMenu(View view) throws IOException {
+        if(this.airline == null) {
+            prettyPrintFlights(view, null);
+        }
+        else
+            setContentView(R.layout.search_flights);
     }
 
     public void searchFlights(View view) throws IOException {
-        // change it later to search flights
+        EditText et_src = findViewById(R.id.search_src);
+        EditText et_dest = findViewById(R.id.search_dest);
+        String src = et_src.getText().toString();
+        String dest = et_dest.getText().toString();
+
+        if ((src == null || src.equals("")) && (dest == null || dest.equals(""))) {
+            displayPopupMessage(view, "No airports were specified, all flights will be displayed.");
+            this.prettyPrintFlights(view, this.airline);
+        }
+        else if (src == null || src.equals("")) {
+            this.displayPopupMessage(view, "Source has to be a 3 letter airport code");
+        }
+        else if (dest == null || dest.equals("")) {
+            this.displayPopupMessage(view, "Destination has to be a 3 letter airport code");
+        }
+        else {
+            if(!verifyAirportCode(src)) {
+                displayPopupMessage(view, "Invalid airport source code!");
+                return;
+            }
+            if(!verifyAirportCode(dest)) {
+                displayPopupMessage(view, "Invalid airport destination code!");
+                return;
+            }
+            Airline air = constructAirlineSearch(src, dest);
+            this.prettyPrintFlights(view, air);
+        }
+
+    }
+
+    private Airline constructAirlineSearch(String src, String dest) {
+        if(this.airline == null) {
+            return null;
+        }
+        Collection<Flight> flights = this.airline.getFlights();
+        Airline air = new Airline(this.airline.getName());
+        for(Flight flight : flights) {
+            if(flight.getSource().equals(src) && flight.getDestination().equals(dest)) {
+                air.addFlight(flight);
+            }
+        }
+        return air;
+
+    }
+
+    public void prettyPrintFlights(View view, Airline air) throws IOException {
         setContentView(R.layout.pretty_print);
         TextView tv = findViewById(R.id.prettyPrint);
+
+        if(this.airline == null) {
+            tv.setText("The airline does not exist. Go to the \"Add a flight\" menu to create an airline.");
+            return;
+        }
 
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         PrettyPrinter pp = new PrettyPrinter(pw);
-        pp.dump(this.airline);
+        pp.dump(air);
 
         tv.setText(sw.toString());
         tv.setMovementMethod(new ScrollingMovementMethod());
-//        this.mainMenu(view);
     }
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        this.airMan = new Intent(this, AirlineManager.class);
-//    }
-//
-//    public void printReadme(View view) {
-//        setContentView(R.layout.readme);
-//    }
-//
-//    public void mainMenu(View view) {
-//        setContentView(R.layout.activity_main);
-//    }
-//
-//    public void goToAddFlightMenu(View view) {
-////        Intent intent = new Intent(this, AirlineManager.class);
-//        startActivity(this.airMan);
-//    }
 }
